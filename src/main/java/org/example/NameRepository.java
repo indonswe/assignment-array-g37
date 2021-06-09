885b810d4b00f6ef6df5a4403378e0c953f6dbd5
indonswe <afc2002afc@hotmail.com>
2021-06-09 09:49:52 +0200

    newArray

--- src/main/java/org/example/NameRepository.java
+++ src/main/java/org/example/NameRepository.java
@@ -3,7 +3,7 @@ package org.example;
 
 
 public class NameRepository {
-    static String[] names  = new String[5];
+    static String[] names  = new String[0];
 
     public static int getSize(){
         int elements=names.length;
@@ -12,5 +12,19 @@ public class NameRepository {
     public static void clear(){
        names  = new String[names.length];
     }
-    
+    public static boolean add(final String fullName){
+        int nl = names.length+1;
+        String [] newArray = new String[nl];
+        for(String i:names){
+            if (i.equals(fullName)) return false;
+        }
+        newArray[nl-1] = fullName;
+        for (int i = 0;i < names.length; i++)
+        {
+            newArray[i] = names[i];
+        }
+        newArray[nl-1] = fullName;
+        names = newArray;
+        return true;
+    }
 }
